6433701cba759f14bb132f9611196dc0df40d6c0
Tim Booher <ads@theboohers.org>
2010-08-10 21:39:14 -0400

    fixed another error

--- app/models/coach.rb
+++ app/models/coach.rb
@@ -7,7 +7,7 @@ class Coach < ActiveRecord::Base
   def blocks_this_month(a_date)
     blocks_this_month = {}
     EventType.all.each do |et|
-      blocks_this_month[et.name.to_sym] = self.events.select{|e| e.start_time >= a_date.beginning_of_month.start_of_day && e.start_time <= a_date.end_of_month.end_of_day && e.event_type == et}.size
+      blocks_this_month[et.name.to_sym] = self.events.select{|e| e.start_time >= a_date.beginning_of_month.beginning_of_day && e.start_time <= a_date.end_of_month.end_of_day && e.event_type == et}.size
     end
     return blocks_this_month
   end
